4f1943aed127de52ae0bcef576edf908a2f382c1
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-16 22:36:57 -0300

    Adding calculator project

---
+++ calculadora/README.md
@@ -0,0 +1,10 @@
+## Calculadora com JavaScript
+
+Uma calculadora que suporta operações simples com javascript;
+
+Desenvolvida para a disciplina de **Programação para internet 2**
+
+<h2 align="center">Layout demo</h2>
+<p align="center">
+    <img src="./demo/Calculadora.gif"/>
+</p>
---
+++ calculadora/js/index.js
@@ -0,0 +1,51 @@
+function addValue(value) {
+  let operation = "";
+  operation = value;
+  const display = document.getElementById("display");
+  const operations = document.getElementById("operation");
+  if (["+", "-", "*", "/"].includes(operation)) {
+    display.innerHTML = "";
+  } else display.innerHTML += operation;
+  operations.innerHTML += operation;
+}
+
+function calculate() {
+  const operation = document.getElementById("operation");
+  let numbers = operation.innerHTML.split(/[+*\/-]/g);
+  let operator = operation.innerHTML.charAt(
+    operation.innerHTML.search(/[+*\/-]/g)
+  );
+  let result = 0;
+  if (["+", "-", "*", "/"].includes(operator) && numbers.length === 2) {
+    switch (operator) {
+      case "+":
+        result = parseFloat(numbers[0]) + parseFloat(numbers[1]);
+        break;
+      case "-":
+        result = parseFloat(numbers[0]) - parseFloat(numbers[1]);
+        break;
+      case "*":
+        result = parseFloat(numbers[0]) * parseFloat(numbers[1]);
+        break;
+      case "/":
+        result = parseFloat(numbers[0]) / parseFloat(numbers[1]);
+        break;
+    }
+    document.getElementById("display").innerHTML = result;
+  } else {
+    document.getElementById("display").innerHTML = "Operação inválida ou não suportada no momento";
+  }
+}
+
+function removeCharacter() {
+  const operations = document.getElementById("operation");
+  operations.innerHTML = operations.innerHTML.substr(
+    0,
+    operations.innerHTML.length - 1
+  );
+}
+
+function purge() {
+  document.getElementById("display").innerHTML = "0";
+  document.getElementById("operation").innerHTML = "";
+}
