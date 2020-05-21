57dff7800879f350eba0c40dd3f758d9ed97c5c2
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-21 16:21:30 -0300

    updating symbols and iterators exercise

--- Es6-exercises/src/symbols-iterators.js
+++ Es6-exercises/src/symbols-iterators.js
@@ -5,3 +5,25 @@ const obj = {
 };
 
 console.log(obj);
+
+const arr = [1, 2, 3, 4];
+
+const obj2 = {
+  values: [...arr],
+  [Symbol.iterator]() {
+    let i = 0;
+    return {
+      next: () => {
+        i++;
+        return {
+          value: this.values[i - 1],
+          done: i > this.values.length
+        }
+      }
+    }
+  }
+};
+
+for(let item of obj2) {
+  console.log(item);
+}
