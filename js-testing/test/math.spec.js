1aa94fd51ec7223ae7d155301e0e2fb520a9126f
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-07-03 23:01:41 -0300

    feat: adding tests with chai

--- js-testing/test/math.spec.js
+++ js-testing/test/math.spec.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
 const Math = require("../src/math.js");
+const expect = require("chai").expect;
 
 describe("Math class", () => {
   it("should sum two numbers", function (done) {
@@ -7,13 +8,27 @@ describe("Math class", () => {
     this.timeout(3000);
 
     math.sum(5, 5, (value) => {
-      assert.equal(value, 10);
+      expect(value).to.equal(10);
       done();
     });
   });
 
   it("should multiply two numbers", function () {
     const math = new Math();
-    assert.equal(math.multiply(2, 5), 10);
+    expect(math.multiply(2, 5)).to.equal(10);
+  });
+
+  it("should have property", function () {
+    const obj = {
+      name: "Guilherme Gules",
+    };
+    expect(obj).to.have.property(name);
+  });
+
+  it("should have property name value", function () {
+    const obj = {
+      name: "Guilherme Gules",
+    };
+    expect(obj).to.have.property(name).equal("Guilherme Gules");
   });
 });
