240211426486e1e27d706cd365cfda07eb469605
Guilherme Gules <guilherme.moreira@ilegra.com>
2021-11-10 14:25:27 -0300

    feat: add one more example about generators

--- generators/src/index.js
+++ generators/src/index.js
@@ -9,9 +9,13 @@ function* hello() {
 
 const it = hello();
 
+console.log("Example 1 \n");
+
 console.log(it.next());
 console.log(it.next());
 console.log(it.next("Outside!"));
+
+console.log("\nExample 2 \n");
 
 function* naturalNumbers() {
   let number = 0;
@@ -30,6 +34,8 @@ console.log(natural.next());
 console.log(natural.next());
 console.log(natural.next());
 
+console.log("\nExample 3 \n");
+
 const object = {
   values: [1, 2, 3, 4],
   *[Symbol.iterator]() {
@@ -42,3 +48,22 @@ const object = {
 for (let value of object) {
   console.log(value);
 }
+
+console.log("\nExample 4 \n");
+
+const array = ["0", "1", "4", "a", "9", "c", "16"];
+const myObject = {
+  [Symbol.iterator]: function* () {
+    for (let index of array) {
+      yield `${index}`;
+    }
+  },
+};
+
+const all = [...myObject]
+  .map((index) => parseInt(index, 10))
+  .map(Math.sqrt)
+  .filter((index) => index < 5)
+  .reduce((prev, act) => prev + act);
+
+console.log(all);
